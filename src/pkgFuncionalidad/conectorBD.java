5049e25fd4266c0da8989591ca219f3060b47e67
elCatmon <89800915+srCatmon@users.noreply.github.com>
2022-11-25 08:38:31 -0600

    conector

--- src/pkgFuncionalidad/conectorBD.java
+++ src/pkgFuncionalidad/conectorBD.java
@@ -4,10 +4,24 @@
  */
 package pkgFuncionalidad;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author cesar
  */
 public class conectorBD {
-    
+        public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/upp_pos_cs_2022", "POS_SIS", "159753123");
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return con;
+    }
 }
