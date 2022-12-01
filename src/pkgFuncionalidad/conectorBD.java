7d368cb47bbbc8c2257309a32eff7f26f95fba4c
elCatmon <89800915+srCatmon@users.noreply.github.com>
2022-12-01 08:17:43 -0600

    POS UPP

--- src/pkgFuncionalidad/conectorBD.java
+++ src/pkgFuncionalidad/conectorBD.java
@@ -15,8 +15,10 @@ public class conectorBD {
     public static Connection getConnection() {
         Connection con = null;
         try {
+            //driver para la conexion con mysql
             Class.forName("org.gjt.mm.mysql.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3308/upp_pos_cs_2022?user=POS_SIS&password=159753123");
+            //
+            con = DriverManager.getConnection("jdbc:mysql://192.168.8.89:3308/upp_pos_cs_2022?user=POS_SIS&password=159753123");
         } catch (ClassNotFoundException | SQLException e) {
             JOptionPane.showMessageDialog(null, e);
         }
